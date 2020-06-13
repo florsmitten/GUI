43b30579faafba8d2ebfcf384a2eff4622fc37e7
Flor <Flor@192.168.5.31>
2020-06-13 01:33:54 -0300

    ..

---
+++ GUI-JRadioButton/src/main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		RadioButtonFrame rbf = new RadioButtonFrame();
+		rbf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rbf.setSize(100,150);
+		rbf.setVisible(true);
+		} 
+
+}
